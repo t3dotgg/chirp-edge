d08fb6b37f0514511b6a2a12278207b035e0d21d
Theo Browne <me@t3.gg>
2023-03-01 01:41:35 -0800

    hide link for now

--- src/app/tweets.tsx
+++ src/app/tweets.tsx
@@ -14,24 +14,24 @@ type TweetType = {
 export const TweetView = (props: { tweet: TweetType }) => {
   return (
     <div className="border-t border-zinc-700 p-4 shadow-lg">
-      <Link href={`/post/${props.tweet.id}`}>
-        <div className="flex items-center">
-          <img
-            src={props.tweet.user.profileImageUrl}
-            alt="Profile"
-            className="h-14 w-14 rounded-full"
-          />
-          <div className="ml-3 flex flex-col text-2xl">
-            <div className="text-base font-bold text-slate-300">
-              <span>{`@${props.tweet.user.username}`}</span>
-              <span className="font-thin">{` · ${dayjs(
-                props.tweet.createdAt
-              ).fromNow()}`}</span>
-            </div>
-            <div className="text-slate-300">{props.tweet.content}</div>
+      {/* <Link href={`/post/${props.tweet.id}`}> */}
+      <div className="flex items-center">
+        <img
+          src={props.tweet.user.profileImageUrl}
+          alt="Profile"
+          className="h-14 w-14 rounded-full"
+        />
+        <div className="ml-3 flex flex-col text-2xl">
+          <div className="text-base font-bold text-slate-300">
+            <span>{`@${props.tweet.user.username}`}</span>
+            <span className="font-thin">{` · ${dayjs(
+              props.tweet.createdAt
+            ).fromNow()}`}</span>
           </div>
+          <div className="text-slate-300">{props.tweet.content}</div>
         </div>
-      </Link>
+      </div>
+      {/* </Link> */}
     </div>
   );
 };
