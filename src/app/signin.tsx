c81c17b89dd8147c924aa43d9cca6878974ed855
Theo Browne <me@t3.gg>
2023-03-06 16:25:09 -0800

    Working sign in ty james

--- src/app/signin.tsx
+++ src/app/signin.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 // Importing anything from @clerk/nextjs on client appears to break edge due to buffer calls
-import { useSignIn } from "@clerk/nextjs";
+import { useClerk } from "@clerk/nextjs/app-beta/client";
 
 export const CustomSignIn = () => {
-  const s = useSignIn();
+  const { openSignIn } = useClerk();
 
-  return <button onClick={() => null}>Sign In</button>;
+  return <button onClick={() => openSignIn()}>Sign In</button>;
 };
