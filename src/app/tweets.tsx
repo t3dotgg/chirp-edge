ba6b49d2385b635b9f14e1d13adc025ca09b590a
Theo Browne <me@t3.gg>
2023-03-07 16:14:42 -0800

    I'm dumb

--- src/app/tweets.tsx
+++ src/app/tweets.tsx
@@ -46,7 +46,10 @@ export default async function Tweets() {
   const data = rows as TweetType[];
   console.log(data);
   const userIds = data.map((post) => post.authorId);
-  const users = await clerkClient.users.getUserList({ userIds, limit: 100 });
+  const users = await clerkClient.users.getUserList({
+    userId: userIds,
+    limit: 100,
+  });
 
   users.forEach((u) => console.log(u.username));
 
